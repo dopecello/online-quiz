fdb092affdb5a469be666a9a90630d64648d1b8b
dopecello <williamlocke.cello@gmail.com>
2022-07-07 17:17:14 -0400

    game hud working

--- assets/script.js
+++ assets/script.js
@@ -1,78 +1,116 @@
-const question = document.querySelector('#question');
-const progressText = document.querySelector('#progressText');
+const questionText = document.querySelector('#question');
+const questionNumber = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
-const startQuizButton = document.querySelector('#start-quiz');
+const choiceOne = document.querySelector('#choice-one'); 
+const choiceTwo = document.querySelector('#choice-two');
+const choiceThree = document.querySelector('#choice-three');
+const choiceFour = document.querySelector('#choice-four');
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestion = [];
+let currentQuestion = 0
+let score = 0
 
-let questions = [
+const questions = [
     {
+        id: 0,
         question: 'Who is the inventor of JavaScript?',
-        choice1: 'James Gosling',
-        choice2: 'Tim Berners-Lee',
-        choice3: 'Google',
-        choice4: 'Brandon Eich',
-        answer: 4,
+        a: 'James Gosling',
+        b: 'Tim Berners-Lee',
+        c: 'Google',
+        d: 'Brandon Eich',
+        correct: 'Brandon Eich',
     },
     {
+        id: 1,
         question: 'Which data type is not a primitive one?',
-        choice1: 'String',
-        choice2: 'Array',
-        choice3: 'Number',
-        choice4: 'Object',
-        answer: 4,
+        a: 'String',
+        b: 'Array',
+        c: 'Number',
+        d: 'Object',
+        correct: 'd',
     },
     {
+        id: 2,
         question: 'What does DOM stand for?',
-        choice1: 'Dimunated Orientation Module',
-        choice2: 'Document Object Model',
-        choice3: 'Dominant Overload Mitigator',
-        choice4: 'Dollar Oats Meal',
-        answer: 2,
+        a: 'Dimunated Orientation Module',
+        b: 'Document Object Model',
+        c: 'Dominant Overload Mitigator',
+        d: 'Dollar Order Meal',
+        correct: 'b',
     },
     {
-        question: 'What JavaScript mechanism moves variables, classes, or function declarations to the top of their scop before code execution?',
-        choice1: 'Lifting',
-        choice2: 'Bubbling',
-        choice3: 'Hoisting',
-        choice4: 'Hurling',
-        answer: 3,
+        id: 3,
+        question: 'What JavaScript mechanism moves variables, classes, or function declarations to the top of their scope before code execution?',
+        a: 'Lifting',
+        b: 'Bubbling',
+        c: 'Hoisting',
+        d: 'Hurling',
+        correct: 'c',
     },
     {
+        id: 4,
         question: 'What types of web storage exist when working with a browser?',
-        choice1: 'Local Storage',
-        choice2: 'External Storage',
-        choice3: 'Session Storage',
-        choice4: 'Both A + C',
-        answer: 4,
+        a: 'Local Storage',
+        b: 'External Storage',
+        c: 'Session Storage',
+        d: 'Both A + C',
+        correct: 'd',
     },
     {
+        id: 5,
         question: 'What is IIFE?',
-        choice1: 'Immediately Invoked Function Expression',
-        choice2: 'Inner Integer Function Expression',
-        choice3: 'Important Imminent Feature Exploit',
-        choice4: 'Insanely Idiotic Female Entreprenuer',
-        answer: 1,
-    },
-]
+        a: 'Immediately Invoked Function Expression',
+        b: 'Inner Integer Function Expression',
+        c: 'Important Imminent Feature Exploit',
+        d: 'Insanely Idiotic Female Entreprenuer',
+        correct: 'a'
+    }
+];
 
-//timer functions/variables
+function loadQuiz () {
+    let qAndA = questions[currentQuestion]
+    questionText.textContent = qAndA.question
+    choiceOne.textContent = qAndA.a
+    choiceTwo.textContent = qAndA.b
+    choiceThree.textContent = qAndA.c
+    choiceFour.textContent = qAndA.d
+}
 
-var counter = 60
-var countdown = function(){
+loadQuiz();
+
+function changeQuestion (event) {
+    var correct = questions[currentQuestion].correct
+    if (correct !== event.target.textContent) {
+        counter = counter - 5;
+    }
+    currentQuestion++
+    loadQuiz();
+}
+
+let questionTrigger = document.querySelectorAll(".choice-container")
+questionTrigger.forEach(function(answerEl){
+    answerEl.addEventListener('click', changeQuestion)
+})
+
+//timer function
+let counter = 120
+var countdown = function () {
     document.getElementById("timer-text").innerHTML = (counter);
     counter--;
     if (counter === -1) {
         clearInterval(startCountdown)
         window.location.replace("./end.html");
     }
+    else if (currentQuestion === questions.length) {
+        localStorage.setItem("score", counter)
+        window.location.replace("./end.html")
+    }
 };
-
 var startCountdown = setInterval(countdown, 1000);
-countdown(startCountdown);
+
+
+
+
+
+
 
